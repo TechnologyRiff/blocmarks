5c280162797cb468951e69dee7ea398a9623c6c2
technologyRiff <technologyriff@gmail.com>
2015-02-26 00:25:22 -0500

    added spacing to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
          has_many :topics
          has_many :bookmarks, dependent: :destroy
          has_many :likes, dependent: :destroy
+         has_many :bookmarks, through: :likes
 
   def liked(bookmark)
     likes.where(bookmark_id: bookmark.id, user_id: id).first
