e961fc41aa0d27f66eddd36803bf594dc254c4e3
technologyRiff <technologyriff@gmail.com>
2015-02-27 16:54:00 -0500

    trying has many through

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,21 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :bookmark
+
+  def self.bookmarks
+    Bookmark.where( id: pluck(:bookmark_id) )
+  end
+
+  def self.users
+    User.where( id: pluck(:user_id) )
+  end
+
+  def bookmark
+    Bookmark.find(bookmark_id)
+  end
+
+  def user
+    User.find(user_id)
+  end
+  
 end
