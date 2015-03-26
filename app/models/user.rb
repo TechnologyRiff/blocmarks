26d7867cf39966febc5980443e44d6cb613b73aa
Morgan Riffer <technologyriff@gmail.com>
2015-03-26 09:11:56 -0400

    fixed spacing user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :topics
+  has_many :bookmarks, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_bookmarks, through: :likes, source: :bookmark
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-
-         has_many :topics
-         has_many :bookmarks, dependent: :destroy
-         has_many :likes, dependent: :destroy
-         has_many :liked_bookmarks, through: :likes, source: :bookmark
 
   def pending_invite?
     invitation_created_at && !invitation_accepted_at
